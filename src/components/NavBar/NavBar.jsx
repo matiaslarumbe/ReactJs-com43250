086f798ffb1b222b8d3ea2247bec5d9705707025
Matias Larumbe <matias_larumbe@hotmail.com>
2024-07-14 15:35:50 -0300

    Actualizacion de Logo.jpg 1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { CartWidget } from "../CartWidget/CartWidget";
-import logo from "../../assets/img/Logo.jpg";
+import { logo } from "../../assets/img/Logo.jpg";
 // import "./navbar.css";
 
 import { Link } from "react-router-dom";
